fb4a55bb9a7e68f0a3d3d32b90ee56f85b6cc275
Stian Håklev <shaklev@gmail.com>
2013-03-24 02:40:04 +0100

    some more code on log - far from done

--- log.rb
+++ log.rb
@@ -1,12 +1,13 @@
 # encoding: utf-8
 
-# tags arbitrary facts using key-value
-
 $:.push(File.dirname($0))
 require 'library'
 require 'settings'
+require 'yaml'
 require 'Pashua'
 include Pashua
+
+# tags arbitrary facts using key-value
 
 categories = try { YAML::load(File.read('categories.txt')) }
 categories = [""] unless categories
@@ -14,24 +15,31 @@ categories = [""] unless categories
 config = "
 *.title = Log something
 
-  *.title = researchr
-  cb.type = combobox
-  cb.completion = 2
+*.title = loggr
+cb.type = combobox
+cb.completion = 2
 
-  cb.default = start
-  cb.width = 220
-  cb.tooltip = Choose from the list or enter another name
+cb.default = start
+cb.width = 220
+cb.tooltip = Choose from the list or enter another name
 tb.type = textbox
 timelab.type = text
 timelab.default = Time stamp
 time.type = textfield
 time.default = #{Time.now}
-  db.type = cancelbutton
-  db.label = Cancel
-  db.tooltip = Closes this window without taking action" + "\n"
+cancel.type = cancelbutton
+cancel.label = Cancel
+cancel.tooltip = Closes this window without taking action" + "\n"
 
-categories.each {|cat| config << "cb.option = #{cat}" }
+categories.each {|cat| config << "cb.option = #{cat}\n" }
 
-  # insert list of all wiki pages from filesystem into Pashua config
-  pagetmp = pashua_run config
+# insert list of all wiki pages from filesystem into Pashua config
+pagetmp = pashua_run config
+exit if pagetmp['cancel'] == 1
 
+cat = try { pagetmp['cb'].strip }
+unless categories.index(cat)
+  categories << cat
+  categories - [""] # remove empty field if we have categories
+  File.open('categories.txt', 'w') {|f| f << YAML::dump(categories)}
+end
